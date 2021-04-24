37cfa3e164a9a0e4f4fd7ed30f1f628c6b38aba5
Younes <younes@home.com>
2021-04-24 23:17:09 +0000

    adding TPL

--- public/js/startTick.js
+++ public/js/startTick.js
@@ -1,13 +1,16 @@
 window.onload = () => {
   let t = new TimeTracker();
+  t.startEngine();
 
-  $("#btnStart").click(() => {
-    t.startEngine();
-  })
+  $("#tempoTplContainer").load("templates/greetings.htm", function () {
+    $("#simpleHello").tmpl({ timeStamp: [{ title: "Vigie", comment: "anomalies" },{ title: "PDC", comment: "anomalies PDC" }] })
+      .appendTo("#modalBody");
 
-  $("#list-tab>a").each((...value) => {
-    $(value[1]).click((elem) => console.log(elem));
+    $("#list-tab>a").each((...value) => {
+      $(value[1]).click((elem) => console.log(elem));
+    });
   });
+
 
 
 };
