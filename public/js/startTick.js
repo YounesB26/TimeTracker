45bdcca6b8ddd1c978f8e62b2f0579a4d8a2b76a
Younes <younes@home.com>
2021-04-25 22:52:19 +0000

    adding modal management : clic on item, events when shown and closed

--- public/js/startTick.js
+++ public/js/startTick.js
@@ -1,13 +1,32 @@
 window.onload = () => {
   let t = new TimeTracker();
-  t.startEngine();
+  //TODO to activate : t.startEngine();
 
-  $("#tempoTplContainer").load("templates/greetings.htm", function () {
-    $("#simpleHello").tmpl({ timeStamp: [{ title: "Vigie", comment: "anomalies" },{ title: "PDC", comment: "anomalies PDC" }] })
+  //Loading modal template code
+  $("#tempoTplContainer").load("templates/tagModal.htm", function () {
+    $("#tagModalContent").tmpl();
+  });
+
+
+  $("#tagModal").on("hidden.bs.modal", function () {
+    $("#modalBody").empty();
+  });
+
+
+  $("#tagModal").on("show.bs.modal", function () {
+
+    $("#tagModalContent").tmpl({ timeStamp: [{ title: "Vigie", comment: "anomalies" }, { title: "PDC", comment: "anomalies PDC" }] })
       .appendTo("#modalBody");
 
+    $("#tagList>a").each((index, elem) => {
+      $(elem).on("click", () => {
+        $("#tagTitleInput")[0].value = $("#tagTitle_" + index)[0].innerText;
+        $("#tagCommentInput")[0].value = $("#tagComment_" + index)[0].innerText;
+      });
+    });
+
     $("#list-tab>a").each((...value) => {
-      $(value[1]).click((elem) => console.log(elem));
+      $(value[1]).on("click", (elem) => console.log(elem));
     });
   });
 
